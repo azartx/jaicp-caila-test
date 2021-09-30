c795e57f2be722e93346b615b7b19e1fd82ca6e7
Semen <66694997+azartx@users.noreply.github.com>
2021-09-30 18:18:46 +0300

    Update MainScenario.kt

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -4,50 +4,19 @@ import com.justai.jaicf.activator.caila.caila
 import com.justai.jaicf.builder.Scenario
 
 val mainScenario = Scenario {
-    state("start") {
-        activators {
-            regex("/start")
-            intent("Hello")
-        }
-        action {
-            reactions.run {
-                image("https://media.giphy.com/media/ICOgUNjpvO0PC/source.gif")
-                sayRandom(
-                    "Hello! How can I help?",
-                    "Hi there! How can I help you?"
-                )
-                buttons(
-                    "Help me!",
-                    "How are you?",
-                    "What is your name?"
-                )
+state("changeView") {
+            activators {
+                intent("changeView")
+            }
+            action {
+                reactions.say("Перехожу..." )
+                var slot = ""
+                activator.caila?.run {slot = slots["views"].toString()}
+                reactions.aimybox?.response?.action = "changeView"
+                reactions.aimybox?.response?.intent = slot
+
             }
         }
-    }
-
-    state("bye") {
-        activators {
-            intent("Bye")
-        }
-
-        action {
-            reactions.sayRandom(
-                "See you soon!",
-                "Bye-bye!"
-            )
-            reactions.image("https://media.giphy.com/media/EE185t7OeMbTy/source.gif")
-        }
-    }
-
-    state("smalltalk", noContext = true) {
-        activators {
-            anyIntent()
-        }
-
-        action(caila) {
-            activator.topIntent.answer?.let { reactions.say(it) } ?: reactions.go("/fallback")
-        }
-    }
 
     fallback {
         reactions.sayRandom(
